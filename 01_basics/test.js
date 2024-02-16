8c261f9ea2f55bd0de20c9b9b26894654048dfd3
devilal8094 <160167727+devilal8094@users.noreply.github.com>
2024-02-16 09:48:26 +0000

    tr

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -13,3 +13,33 @@ console.log(negValue);
 // consolelog(2**2)
 // consolelog(2/2)
 // consolelog(2%2)
+
+
+console.log("1" + 2);
+console.log(2 + "1");
+console.log(1 + 2 + "3")
+
+
+//  ******************************   Comparison of datatypes  *********************  //
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+
+// console.log("2" > 1);
+// consolelog("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
