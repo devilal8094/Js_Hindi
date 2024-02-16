c52d67077a97818ad92a1414d4f38a30c35cd966
devilal8094 <160167727+devilal8094@users.noreply.github.com>
2024-02-16 08:48:59 +0000

    tr

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,15 @@
 console.log('hello')
 console.log('hello dev baba');
+
+
+
+let value = 3
+let negValue = -3
+console.log(negValue);
+
+// consolelog(2+2)
+// consolelog(2-2)
+// consolelog(2*2)
+// consolelog(2**2)
+// consolelog(2/2)
+// consolelog(2%2)
